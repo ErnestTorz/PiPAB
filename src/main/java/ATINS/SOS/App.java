de683be406f0e933148bbda70647f9dc1d46c979
Ernest Torz <ernesttorz@gmail.com>
2025-05-18 17:17:30 +0200

    ini

---
+++ src/main/java/ATINS/SOS/App.java
@@ -0,0 +1,13 @@
+package ATINS.SOS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
