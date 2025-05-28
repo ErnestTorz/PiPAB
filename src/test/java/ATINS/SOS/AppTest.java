9fe1d2f3d544e87a771c1a391fdf43208aebcdbf
mateusz plewa <plewa-mateusz@o2.pl>
2025-05-28 20:01:08 +0200

    Add DataSource for HSQLDB

--- src/test/java/ATINS/SOS/AppTest.java
+++ src/test/java/ATINS/SOS/AppTest.java
@@ -3,36 +3,44 @@ package ATINS.SOS;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * @return the suite of tests being tested
+     * Oryginalny test wygenerowany przez szablon.
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testApp() {
+        assertTrue(true);
     }
 
     /**
-     * Rigourous Test :-)
+     * Nowy test: sprawdza połączenie z bazą danych HSQLDB i wykonuje proste zapytanie.
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testDatabaseConnection() {
+        try (ClassPathXmlApplicationContext context =
+                     new ClassPathXmlApplicationContext("WEB-INF/applicationContext.xml")) {
+
+            JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate", JdbcTemplate.class);
+            Integer result = jdbcTemplate.queryForObject(
+                    "SELECT 1 FROM INFORMATION_SCHEMA.SYSTEM_USERS", Integer.class);
+
+            assertEquals("Połączenie z bazą nie działa poprawnie", Integer.valueOf(1), result);
+
+        } catch (Exception e) {
+            fail("Błąd połączenia z bazą: " + e.getMessage());
+        }
     }
 }
