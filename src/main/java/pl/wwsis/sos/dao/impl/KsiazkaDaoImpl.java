de6face09fe49d65ca1f5cbc9e5b669cb63e344b
Mateusz Plewa <plewa-mateusz@o2.pl>
2025-06-30 23:51:31 +0200

    Add OcenaDao and OcenaDaoImpl

--- src/main/java/pl/wwsis/sos/dao/impl/KsiazkaDaoImpl.java
+++ src/main/java/pl/wwsis/sos/dao/impl/KsiazkaDaoImpl.java
@@ -1,12 +1,39 @@
-package pl.wwsis.sos.dao;
+package pl.wwsis.sos.dao.impl;
 
+import pl.wwsis.sos.dao.KsiazkaDao;
 import pl.wwsis.sos.model.Ksiazka;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
-public interface KsiazkaDao {
-    Ksiazka findById(Long id);
-    List<Ksiazka> findAll();
-    void save(Ksiazka ksiazka);
-    void delete(Long id);
+@Transactional
+public class KsiazkaDaoImpl implements KsiazkaDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Ksiazka findById(Long id) {
+        return entityManager.find(Ksiazka.class, id);
+    }
+
+    @Override
+    public List<Ksiazka> findAll() {
+        return entityManager.createQuery("SELECT k FROM Ksiazka k", Ksiazka.class).getResultList();
+    }
+
+    @Override
+    public void save(Ksiazka ksiazka) {
+        entityManager.persist(ksiazka);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Ksiazka ksiazka = entityManager.find(Ksiazka.class, id);
+        if (ksiazka != null) {
+            entityManager.remove(ksiazka);
+        }
+    }
 }
--- src/main/java/pl/wwsis/sos/dao/impl/OcenaDaoImpl.java
+++ src/main/java/pl/wwsis/sos/dao/impl/OcenaDaoImpl.java
@@ -1,5 +1,39 @@
 package pl.wwsis.sos.dao.impl;
 
-public class OcenaDaoImpl {
+import pl.wwsis.sos.dao.OcenaDao;
+import pl.wwsis.sos.model.Ocena;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class OcenaDaoImpl implements OcenaDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Ocena findById(Long id) {
+        return entityManager.find(Ocena.class, id);
+    }
+
+    @Override
+    public List<Ocena> findAll() {
+        return entityManager.createQuery("SELECT o FROM Ocena o", Ocena.class).getResultList();
+    }
+
+    @Override
+    public void save(Ocena ocena) {
+        entityManager.persist(ocena);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Ocena ocena = entityManager.find(Ocena.class, id);
+        if (ocena != null) {
+            entityManager.remove(ocena);
+        }
+    }
 }
